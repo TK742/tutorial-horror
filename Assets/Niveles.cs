a07cce7f9b0102e260325db61b8c7d199ac97d5f
TK742 <tomytasca@icloud.com>
2018-05-16 07:22:15 -0300

    1.1.0
    Mejorado el interfaz.
    Arreglado el error del cursor.
    Optimizado cambio de nivel.

--- Assets/Niveles.cs
+++ Assets/Niveles.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class Niveles : MonoBehaviour
 {
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;    
+    }
+
     public void CargarNivel (int nivel)
     {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
         UnityEngine.SceneManagement.SceneManager.LoadScene(nivel);
     }
 
